6bcacc87e9e3a83523c02fbbf6d1887942a22d72
Set1304 <restg07@gmail.com>
2020-04-05 15:23:12 +0300

    add render list

--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,14 @@
 const {Router} = require('express');
+const Courses = require('../models/course')
+
 const router = Router()
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const courses = await Courses.getAll()
     res.render('courses', {
         title: 'Курсы',
-        isCourses: true
+        isCourses: true,
+        courses: courses
     })
 })
 
